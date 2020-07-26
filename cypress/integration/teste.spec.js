47e23e2ccd09ce38baa7ffd4e6ec3b792c3a6fa1
Matheus <matheus_or@hotmail.com>
2020-07-26 19:00:17 -0300

    Projeto desafio parcial

---
+++ cypress/integration/teste.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Test', () => {
+        const url = 'https://codepen.io/choskim/full/RLYebL';
+        const iframeUrl = 'https://s.codepen.io/choskim/fullpage/RLYebL';
+
+        beforeEach(() => {
+            cy.request({
+                method: 'GET',
+                url: iframeUrl,
+                headers: {
+                    Referer: url,
+                    accept: 'text/html',
+                }
+            })
+                .its('body')
+                .then(html => {
+                    cy.document().then(document => {
+                        console.log(html)
+                        document.write(html)
+                        document.close()
+                    })
+                })
+            cy.get('.center-center').should('be.visible')
+        })
+
+        it('Square test', () => {
+            cy.get('.square').click({ release: false })
+        })
+})
